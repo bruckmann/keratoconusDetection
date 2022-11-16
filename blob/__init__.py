4ce08c2187473b69126ec4cdee2249060ece5245
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-11-15 21:22:40 -0300

    Adding return on blob insertion function

--- blob/__init__.py
+++ blob/__init__.py
@@ -2,11 +2,9 @@ import os
 
 from azure.storage.blob import BlobClient
 
-import utils
-
 container_name = os.getenv("CONTAINER_NAME")
 connection_string = os.getenv("CONNECTION_STRING")
 
 def insert_on_blob(file_name, image):
     blob = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=file_name)
-    blob.upload_blob(data=image)
+    return blob.upload_blob(data=image)
