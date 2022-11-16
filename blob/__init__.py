f8c60e005c748750fc398356f816482882396a6a
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-11-15 21:30:59 -0300

    Removing open and async

--- blob/__init__.py
+++ blob/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 
-from azure.storage.blob.aio import BlobClient
+from azure.storage.blob import BlobClient
 
 container_name = os.getenv("CONTAINER_NAME")
 connection_string = os.getenv("CONNECTION_STRING")
@@ -13,7 +13,6 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
 
-async def insert_on_blob(file_name, image):
+def insert_on_blob(file_name, image):
     blob = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=file_name, logging_enable=True)
-    with open(image, 'rb') as data:
-        return await blob.upload_blob(data=data)
+    blob.upload_blob(data=image)
