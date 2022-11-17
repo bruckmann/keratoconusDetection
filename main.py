5f198e585c022e7944296cc8fb50b65a75da515a
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-11-17 09:53:25 -0300

    Adding new blob insert route

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import os
 
+from azure.storage.blob import BlobClient
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from azure.storage.blob import BlobClient
 
 import prediction as pd
 import db
+import utils
+
 
 app = Flask(__name__)
 CORS(app)
 
+
+container_name = os.getenv("CONTAINER_NAME")
+connection_string = os.getenv("CONNECTION_STRING")
+
+
+
 @app.route('/predict/', methods=['POST'])
 async def predict():
-    image_to_normalize = request.files.get('image')
-    image_id = request.form.get('image_id')
+    image = request.files.get('image')
     name = request.form.get('name')
     age = request.form.get('age')
 
-    normalized_image = pd.normalize_image(image_to_normalize)
+    normalized_image = pd.normalize_image(image)
     prediction_result = pd.make_prediction(normalized_image)
     classification_result = pd.classify(prediction_result)
     
+
+
     try:
-        db.insert(name, age, prediction_result, classification_result, image_id)
+        db.insert(name, age, prediction_result, classification_result, file_name)
         return jsonify({'prediction_result': str(prediction_result[0][0]), 'classification_result': classification_result})
     except Exception as e:
         return jsonify({'error': str(e)})
 
 @app.route('/upload/', methods=['GET'])
-def predict_version():
-    return "1.0.0"
+def upload_blob():
+    image = request.files.get('image')
+    name = request.form.get('name')
+    image_id = utils.format_file_name(name)
+    blob_client = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=image_id)
+    blob_client.upload_blob(image)
+
+    return jsonify({'image_id': image_id})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
